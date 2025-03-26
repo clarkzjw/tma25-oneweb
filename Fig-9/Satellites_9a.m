ce1fcede648759692520ebfb8399be7608eb04a8
Jinwei Zhao <clarkzjw@uvic.ca>
2025-03-25 22:55:52 -0700

    add supporting satellite positions for Fig 9

---
+++ Fig-9/Satellites_9a.m
@@ -0,0 +1,15 @@
+% Fig. 9 (a)
+startTime = datetime(2025, 3, 13, 13, 18, 0, 'TimeZone', 'UTC');
+stopTime = startTime + minutes(3);
+sampleTime = 1;
+
+sc = satelliteScenario(startTime, stopTime, sampleTime, 'AutoSimulate', true);
+v = satelliteScenarioViewer(sc, "ShowDetails", true);
+
+sat = satellite(sc, './oneweb_tle_2025-03-12T23_00_00Z.txt');
+
+latitude = 50; 
+longitude = 160; 
+height = 15000000; 
+campos(v, latitude, longitude, height);
+play(sc);
