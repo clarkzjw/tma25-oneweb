eb090c3cdf726e4d990d53aa5a7f587c05ae7360
Jinwei Zhao <clarkzjw@uvic.ca>
2025-03-26 11:47:50 -0700

    add dish, SNP and PoP locations in Fig-9

--- Fig-9/Satellites_9a.m
+++ Fig-9/Satellites_9a.m
@@ -8,8 +8,15 @@ v = satelliteScenarioViewer(sc, "ShowDetails", true);
 
 sat = satellite(sc, './oneweb_tle_2025-03-12T23_00_00Z.txt');
 
-latitude = 50; 
-longitude = 160; 
-height = 15000000; 
+dish = groundStation(sc, "Latitude", 42.014, "Longitude", -93.651, "Name", "OneWeb UT");
+ClewistonGS = groundStation(sc, "Latitude", 26.74865, "Longitude", -81.04928, "Name", "Clewiston SNP");
+SouthburyGS = groundStation(sc, "Latitude", 41.452761, "Longitude", -73.288563, "Name", "Southbury SNP");
+SantaPaulaGS = groundStation(sc, "Latitude", 34.40218, "Longitude", -119.07341, "Name", "Santa Paula SNP");
+AshburnPoP = groundStation(sc, "Latitude", 39.016363, "Longitude", -77.459023, "Name", "Ashburn PoP");
+
+latitude = 42; 
+longitude = -93; 
+height = 10000000; 
 campos(v, latitude, longitude, height);
+
 play(sc);
