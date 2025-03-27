7819fc89876dbcd184c5ee7ae66db76fbbef1d96
Jinwei Zhao <clarkzjw@uvic.ca>
2025-03-27 11:17:38 -0700

    add figure 5 heatmap

--- Fig-5/plot.py
+++ Fig-5/plot.py
@@ -1,6 +1,9 @@
+# flake8: noqa: E501
+
 import sys
 
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 import datetime
 
@@ -17,22 +20,56 @@ plt.rcParams.update({"xtick.labelsize": fontsize})
 plt.rcParams.update({"ytick.labelsize": fontsize})
 
 
-filename = "../data/ping-oneweb-gw-arawireless-250107-075911.txt"
-ts, rtt = load_ping(filename)
+def plot_cdf(ts, rtt):
+    start = ts[0]
+    end = start + datetime.timedelta(hours=24)
 
-start = ts[0]
-end = start + datetime.timedelta(hours=24)
+    series = pd.Series(rtt, index=ts)
+    series = series[start:end]
 
-series = pd.Series(rtt, index=ts)
-series = series[start:end]
+    figsize = (5, 4)
+    fig = plt.figure(figsize=figsize)
+    ax = fig.add_subplot(111)
+    ax.ecdf(series.values, label="RTT")
+    ax.set_xlabel("RTT (ms)")
+    ax.set_ylabel("CDF")
+    ax.set_xlim(0, 300)
+    plt.tight_layout()
+    plt.savefig("iowa_rtt_cdf.eps")
+    plt.savefig("iowa_rtt_cdf.png")
 
-figsize = (5, 4)
-fig = plt.figure(figsize=figsize)
-ax = fig.add_subplot(111)
-ax.ecdf(series.values, label="RTT")
-ax.set_xlabel("RTT (ms)")
-ax.set_ylabel("CDF")
-ax.set_xlim(0, 300)
-plt.tight_layout()
-plt.savefig("iowa_rtt_cdf.eps")
-plt.savefig("iowa_rtt_cdf.png")
+
+def plot_heatmap(ts, rtt):
+    date = "2025-01-12"
+    figsize = (10, 8)
+    fontsize = 26
+
+    plt.rcParams.update({"font.size": fontsize})
+    plt.rcParams.update({"axes.labelsize": fontsize})
+    plt.rcParams.update({"xtick.labelsize": fontsize})
+    plt.rcParams.update({"ytick.labelsize": fontsize})
+
+    df = pd.DataFrame({"Datetime": pd.to_datetime(ts), "Value": rtt})
+    df["Date"] = df["Datetime"].dt.date
+    df["Hour"] = df["Datetime"].dt.hour
+    df["Minute"] = df["Datetime"].dt.minute
+
+    df = df[df["Date"] == pd.Timestamp(date).date()]
+    df_grouped = df.groupby(["Hour", "Minute"]).min().reset_index()
+
+    heatmap = df_grouped.pivot(index="Hour", columns="Minute", values="Value")
+
+    plt.figure(figsize=figsize)
+    sns.heatmap(heatmap, cmap="viridis", cbar_kws={"label": "minRTT (ms)"})
+    plt.xlabel("Minute")
+    plt.ylabel("Hour of Day")
+    plt.tight_layout()
+    plt.savefig("iowa_heatmap.eps")
+    plt.savefig("iowa_heatmap.png")
+
+
+if __name__ == "__main__":
+    filename = "../data/ping-oneweb-gw-arawireless-250107-075911.txt"
+    ts, rtt = load_ping(filename)
+    plot_cdf(ts, rtt)
+    plot_heatmap(ts, rtt)
